1c2f5879ed163188b2aaf31eb8759675b21bfd11
EilerGomez <80438705+EilerGomez@users.noreply.github.com>
2021-07-20 18:55:26 -0500

    cambie el tiempo de 600 a 700

--- Hola.java
+++ Hola.java
@@ -3,7 +3,7 @@ public class Hola{
         
         for(int i=0; i<=50; i++){
             try{
-                Thread.sleep(600);
+                Thread.sleep(700);
                 System.out.println("Desencriptando..." + i+"%");
             }catch(InterruptedException e){
                 System.out.println("Error en parar el programa "+ e);
