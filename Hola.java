531f5b7fbf1e0cd17a2bc0c8a547d5e96a05c713
EilerGomez <80438705+EilerGomez@users.noreply.github.com>
2021-07-20 19:11:17 -0500

    Inicie I en 4

--- Hola.java
+++ Hola.java
@@ -1,7 +1,7 @@
 public class Hola{
     public static void main(String args[]){
         
-        for(int i=3; i<=50; i++){
+        for(int i=4; i<=50; i++){
             try{
                 Thread.sleep(200);
                 System.out.println("Desencriptando..." + i+"%");
